c6d012add7b5574147e1960f6f1660364d8b4f12
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-09-05 21:31:33 +0300

    finished the lab

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <>
+                <br></br>
+                <br></br>
+                <small>Author: Hasnaa Al-Habahbeh</small>
+            </>
+        )
+    }
+}
+
+export default Footer
